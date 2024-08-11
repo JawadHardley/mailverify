66cc536cbb07216174a5d401f532a3a223d1c3ca
JawadHardley <jawadcharls@gmail.com>
2024-08-11 11:56:40 +0300

    Gmail intergrated part 2

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/dashboard', function () {
 
 
 //to work with user settings
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
