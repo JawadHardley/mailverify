8a71b870e88ab6c5e2eec1141171e81e6f02327c
JawadHardley <jawadcharls@gmail.com>
2024-08-11 11:20:26 +0300

    Gmail intergrated

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -122,7 +122,3 @@
     <script src="./dist/js/demo.min.js?1720208459" defer></script>
   </body>
 </html>
-
-<a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-    {{ __('Already registered?') }}
-</a>
